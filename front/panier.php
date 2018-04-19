b76e76194821d59107841c0624649b31eaa0a9f0
Fabien Brive <fabienbrive.pro@gmail.com>
2018-04-19 17:46:11 +0200

    entrée en base commande

---
+++ front/panier.php
@@ -0,0 +1,28 @@
+<?php 
+
+require_once('../inc/init.inc.php');
+
+// ----------------------- TRAITEMENT ----------------------------------------
+
+// traitement de la commande avec entrée en base des données dans la table commande et pour le moment une redirection vers la page Boutique
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ----------------------- AFFICHAGE ----------------------------------------
+
+require_once('../inc/haut.inc.php');
+
+echo $c;
+
+require_once('../inc/bas.inc.php');
